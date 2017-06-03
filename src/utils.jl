7b100f2ce96b8cdb0493e7d38d203be11ebf0119
Mladen Kolar <mkolar@gmail.com>
2017-06-03 18:19:10 -0500

    moving util functions here

--- src/utils.jl
+++ src/utils.jl
@@ -39,3 +39,83 @@ function proxL1Fused{T <: AbstractFloat}(x::T, y::T, λ1::T, λ2::T, γ::T=one(T
   t = λ1 * γ
   (shrink(u, t), shrink(v, t))
 end
+
+
+
+####################################
+#
+# helper functions
+#
+####################################
+
+function A_mul_B_row{T<:AbstractFloat}(A::AbstractMatrix{T}, b::AbstractVector{T}, row::Int64)
+  n, p = size(A)
+  ((p == length(b)) && (1 <= row <= n)) || throw(DimensionMismatch())
+
+  v = zero(T)
+  for i=1:p
+    @inbounds v += A[row, i] * b[i]
+  end
+  v
+end
+
+function A_mul_B_row{T<:AbstractFloat}(A::AbstractMatrix{T}, b::SparseVector{T}, row::Int64)
+  n, p = size(A)
+  ((p == length(b)) && (1 <= row <= n)) || throw(DimensionMismatch())
+
+
+  nzval = SparseArrays.nonzeros(b)
+  rowval = SparseArrays.nonzeroinds(b)
+  v = zero(T)
+  for i=1:length(nzval)
+    @inbounds v += A[row, rowval[i]] * nzval[i]
+  end
+  v
+end
+
+function A_mul_B_row{T<:AbstractFloat}(A::AbstractMatrix{T}, b::SparseIterate{T}, row::Int64)
+  n, p = size(A)
+  ((p == length(b)) && (1 <= row <= n)) || throw(DimensionMismatch())
+
+  v = zero(T)
+  @inbounds for icoef = 1:nnz(b)
+      v += A[row, b.nzval2full[icoef]] * b.nzval[icoef]
+  end
+  v
+end
+
+
+function At_mul_B_row{T<:AbstractFloat}(A::AbstractMatrix{T}, b::StridedVector{T}, row::Int64)
+  n, p = size(A)
+  ((n == length(b)) && (1 <= row <= p)) || throw(DimensionMismatch())
+
+  v = zero(T)
+  for i=1:n
+    @inbounds v += A[i, row] * b[i]
+  end
+  v
+end
+
+function At_mul_B_row{T<:AbstractFloat}(A::AbstractMatrix{T}, b::SparseVector{T}, row::Int64)
+  n, p = size(A)
+  ((n == length(b)) && (1 <= row <= p)) || throw(DimensionMismatch())
+
+  nzval = SparseArrays.nonzeros(b)
+  rowval = SparseArrays.nonzeroinds(b)
+  v = zero(T)
+  for i=1:length(nzval)
+    @inbounds v += A[rowval[i], row] * nzval[i]
+  end
+  v
+end
+
+function At_mul_B_row{T<:AbstractFloat}(A::AbstractMatrix{T}, b::SparseIterate{T}, row::Int64)
+  n, p = size(A)
+  ((n == length(b)) && (1 <= row <= n)) || throw(DimensionMismatch())
+
+  v = zero(T)
+  @inbounds for icoef = 1:b.nnz
+      v += A[b.nzval2full[icoef], row] * b.nzval[icoef]
+  end
+  v
+end
